f6b17bfdb0ef20a74aa32eb902725fbbc2d70154
rodia <alejandro.ayala@onecluster.org>
2025-04-06 20:53:46 -0500

    fix: Abstract CinemaReserve

--- Lab2/CinemaReservation/tst_cinemareservation.cpp
+++ Lab2/CinemaReservation/tst_cinemareservation.cpp
@@ -6,19 +6,31 @@
 
 using namespace testing;
 
-TEST(Room, RoomTest){
-    Room room(15, 20);
-    EXPECT_EQ(room.getRows(), 15);
-    EXPECT_EQ(room.getNumbers(), 20);
-    EXPECT_EQ(room.getState(), 'o');
+class CinemaReserve : public ::testing::Test {
+protected:
+    Room* room;
 
-    room.printRoom();
+    void SetUp() override {
+        room = new Room(15, 20);
+    }
 
-}
+    void TearDown() override {
+        delete room;
+    }
+};
+
+TEST_F(CinemaReserve, CreateRoomTest){
+    EXPECT_EQ(room->getRows(), 15);
+    EXPECT_EQ(room->getNumbers(), 20);
+    EXPECT_EQ(room->getState(), 'o');
+
+    room->printRoom();
+
+};
 
 TEST(Seat, SeatTest){
     Seat seat(1, 10);
     EXPECT_EQ(seat.getRow(), 1);
     EXPECT_EQ(seat.getNumber(), 10);
     EXPECT_EQ(seat.getState(), '-');
-}
+};
