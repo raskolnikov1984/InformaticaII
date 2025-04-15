e68ad4fdf1612830ab02e46e4d3709d71c89172b
Rodia <alejandro.ayala@onecluster.org>
2025-04-15 00:20:32 -0500

    feat: Add test cases bitwise operators XOR, <<, >> issue raskolnikov1984/InformaticaII#9

---
+++ DecryptImage/tst_bitwiseoperators.cpp
@@ -0,0 +1,47 @@
+#include <gmock/gmock-matchers.h>
+#include <gtest/gtest.h>
+#include <bitset>
+
+using namespace testing;
+using namespace std;
+
+TEST(BitWiseOperators, BitWiseOperatorsXOR)
+{
+    /*
+    12 = 00001100 (In Binary)
+    25 = 00011001 (In Binary)
+
+         Bitwise XOR Operation of 12 and 25
+         00001100
+     ^   00011001
+         _________
+        00010101  = 21 (In decimal)
+    */
+    EXPECT_EQ(12 ^ 25, 21);
+    EXPECT_EQ(bitset<8>(12) ^ bitset<8>(25), bitset<8>(21));
+    EXPECT_EQ(0b00001100 ^ 0b00011001, 0b00010101);
+}
+
+TEST(BitWiseOperators, BitWiseOperatorsShiftLeft)
+{
+    /*
+    N << m = [ N << (m-1) ] * 2
+    */
+
+    EXPECT_EQ(212 << 0, 212);
+    EXPECT_EQ(212 << 1, 424);
+    EXPECT_EQ(212 << 2, 848);
+    EXPECT_EQ(212 << 3, 1696);
+}
+
+TEST(BitWiseOperators, BitWiseOperatorsShiftRight)
+{
+    /*
+    N >> m = [ N >> (m-1) ] / 2
+    */
+
+    EXPECT_EQ(212 >> 0, 212);
+    EXPECT_EQ(212 >> 1, 106);
+    EXPECT_EQ(212 >> 2, 53);
+    EXPECT_EQ(212 >> 3, 26);
+}
