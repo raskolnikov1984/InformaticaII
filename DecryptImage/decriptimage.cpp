2594e899b9d66ecf4fb8bceadd482ed611079427
Rodia <alejandro.ayala@onecluster.org>
2025-04-18 03:32:54 -0500

    feat(WIP): raskolnikov1984/InformaticaII#11

--- DecryptImage/decriptimage.cpp
+++ DecryptImage/decriptimage.cpp
@@ -1,6 +1,9 @@
 #include "decriptimage.h"
+#include "bitwiseoperators.h"
 
-DecriptImage::DecriptImage() {}
+DecriptImage::DecriptImage() {
+    BitWiseOperators bitwiseOperators;
+}
 
 unsigned char* DecriptImage::loadPixelsBeforeStep(unsigned int* loadSeedMasking, unsigned char* pixelDataMask, int dataSize){
     unsigned char* pixelData = new unsigned char[dataSize];
@@ -10,4 +13,42 @@ unsigned char* DecriptImage::loadPixelsBeforeStep(unsigned int* loadSeedMasking,
     }
 
     return pixelData;
+}
+
+
+bool DecriptImage::isXOR(unsigned char a, unsigned char b, unsigned char& x) {
+    x = a ^ b;
+    return true;
+}
+
+bool DecriptImage::isRotationLeft(unsigned char a, unsigned char b, int& n) {
+    for (n = 1; n < 8; ++n) {
+        if (((a >> n) | (a << (8 - n))) & 0xFF == b)
+            return true;
+    }
+    return false;
+}
+
+bool DecriptImage::isRotationRight(unsigned char a, unsigned char b, int& n) {
+    for (n = 1; n < 8; ++n) {
+        if (((a >> n) | (a << (8 - n))) & 0xFF == b)
+            return true;
+    }
+    return false;
+}
+
+bool DecriptImage::isShiftRight(unsigned char a, unsigned char b, int& n) {
+    for (n = 1; n < 8; ++n) {
+        if ((a >> n) == b)
+            return true;
+    }
+    return false;
+}
+
+bool DecriptImage::isShiftLeft(unsigned char a, unsigned char b, int& n) {
+    for (n = 1; n < 8; ++n) {
+        if ((a >> n) == b)
+            return true;
+    }
+    return false;
 }
--- DecryptImage/decriptimage.h
+++ DecryptImage/decriptimage.h
@@ -1,6 +1,7 @@
 #ifndef DECRIPTIMAGE_H
 #define DECRIPTIMAGE_H
 
+
 class DecriptImage
 {
 public:
@@ -8,6 +9,16 @@ public:
 
     unsigned char* loadPixelsBeforeStep(
         unsigned int* loadSeedMasking, unsigned char* pixelDataMask, int dataSize);
+
+    bool isXOR(unsigned char a, unsigned char b, unsigned char& x);
+
+    bool isShiftLeft(unsigned char a, unsigned char b, int& n);
+
+    bool isShiftRight(unsigned char a, unsigned char b, int& n);
+
+    bool isRotationLeft(unsigned char a, unsigned char b, int& n);
+
+    bool isRotationRight(unsigned char a, unsigned char b, int& n);
 };
 
 #endif // DECRIPTIMAGE_H
