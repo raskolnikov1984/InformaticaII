055e918c1cbab1c61b04a41f296bb4c22ae0ddd0
rodia <alejandro.ayala0@gmail.com>
2025-09-25 20:25:34 -0300

    clean

--- DesafioI/main.cpp
+++ DesafioI/main.cpp
@@ -1,34 +1,38 @@
 #include <iostream>
 #include "encriptacion.h"
 #include "files.h"
-// #include "rle.h"
+#include <iomanip> // Para std::setw y std::setfill
 
 using namespace std;
 
+
 int main()
 {
-    const char archivo_encriptado[] = "/home/rodia/Desktop/04-UdeA/InformaticaII/DesafioI/dataset/Encriptado3.txt";
-    const char archivo_pista[] = "/home/rodia/Desktop/04-UdeA/InformaticaII/DesafioI/dataset/pista3.txt";
+    const char archivo_encriptado[] = "/home/rodia/Desktop/04-UdeA/InformaticaII/DesafioI/dataset/Encriptado1.txt";
+    const char archivo_pista[] = "/home/rodia/Desktop/04-UdeA/InformaticaII/DesafioI/dataset/pista1.txt";
     const char* encriptado = leerArchivo(archivo_encriptado);
     const char* pista = leerArchivo(archivo_pista);
+    char* texto_desencriptado;
 
     int tamano = calcularTamanoArchivo(archivo_encriptado);
 
     int metodo, n_out, K_out;
 
-    char* texto_desencriptado;
-
     metodo = detectarMetodoYParametros(
         encriptado, tamano, pista, n_out, K_out, texto_desencriptado);
 
     cout << "=======================================" << endl;
-    cout << "TEXTO DESENCRIPTADO" << endl;
+    cout << "          TEXTO DESENCRIPTADO          " << endl;
     cout << "=======================================" << endl;
+    cout << endl;
     cout << texto_desencriptado << endl;
+    cout << endl;
     cout << "=======================================" << endl;
-    cout << "Metodo: " << metodo << endl;
-    cout << "n OUT: " << n_out << endl;
-    cout << "K out: " << K_out << endl;;
+    cout << endl;
+    cout << left << setw(15) << "Metodo:" << metodo << endl;
+    cout << left << setw(15) << "n OUT:" << n_out << endl;
+    cout << left << setw(15) << "K out:" << K_out << endl;
+    cout << endl;
     cout << "=======================================" << endl;
 
 
